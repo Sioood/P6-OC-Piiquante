164a32bc75c360a45f52094a9783b6558819cb48
Siod <theodupontpro@gmail.com>
2022-04-19 17:38:55 +0200

    fix error

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,5 +1,4 @@
 const Sauces = require("../models/sauces");
-const fs = require("fs");
 
 // get all sauces
 
@@ -24,9 +23,7 @@ exports.createSauce = (req, res, next) => {
   delete sauceObject._id;
   const sauce = new Sauces({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get("host")}/images/${
-      req.file.filename
-    }`,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   });
   sauce
     .save()
@@ -51,44 +48,30 @@ exports.likeSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file
-    ? {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${
-          req.file.filename
-        }`,
-      }
-    : { ...req.body };
-  Sauces.updateOne(
-    { _id: req.params.id },
-    { ...sauceObject, _id: req.params.id }
-  )
+  const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Sauces.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: "Objet modifié" }))
     .catch((error) => res.status(403).json({ error }));
 };
 
 exports.deleteSauce = (req, res, next) => {
-  Sauces.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      if (!sauce) {
-        res.status(404).json({
-          error: new Error("No such Sauces!"),
-        });
-      }
-      if (sauce.userId !== req.auth.userId) {
-        res.status(400).json({
-          error: new Error("Unauthorized request!"),
-        });
-      }
-      Sauces.deleteOne({ _id: req.params.id });
-      const filename = sauce.imageUrl.split("/images/")[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauces.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: "Objet supprimé !" }))
-          .catch((error) => res.status(400).json({ error }));
+  Sauces.findOne({ _id: req.params.id }).then((sauce) => {
+    if (!sauce) {
+      res.status(404).json({
+        error: new Error("No such Sauces!"),
       });
-    })
-    .catch((error) => res.status(500).json({ error }));
+    }
+    if (sauce.userId !== req.auth.userId) {
+      res.status(400).json({
+        error: new Error("Unauthorized request!"),
+      });
+    }
+    Sauces.deleteOne({ _id: req.params.id })
+      .then(() => res.status(200).json({ message: "Item Deleted !" }))
+      .catch((error) => res.status(400).json({ error }));
+  });
 };
-
-exports.deleteThing = (req, res, next) => {};
